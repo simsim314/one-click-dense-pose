10029a1eccd0ac7e7f8b29d89b8cb63fbde347ab
simsim314 <simsim314@gmail.com>
2024-07-11 08:14:02 +0300

    Update detect_np.py

--- detect_np.py
+++ detect_np.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 import sys
 import numpy as np
 from concurrent.futures import ThreadPoolExecutor
+import torch
 
 def get_image_files(input_folder):
     valid_extensions = ('.jpg', '.jpeg', '.png', '.bmp')
@@ -24,12 +25,24 @@ def create_output_structure(input_folder, output_folder, image_file):
         os.makedirs(output_image_dir)
     return output_image_path
 
-def process_image(image_file, input_folder, output_folder, predictor):
+def rescale_image(image, max_dim=512):
+    height, width = image.shape[:2]
+    if max(height, width) > max_dim:
+        scaling_factor = max_dim / max(height, width)
+        new_size = (int(width * scaling_factor), int(height * scaling_factor))
+        image = cv2.resize(image, new_size, interpolation=cv2.INTER_AREA)
+    return image
+
+def process_image(image_file, input_folder, output_folder, predictor, rescale):
     image_path = image_file
     frame = cv2.imread(image_path)
     if frame is None:
         logger.warning(f"Failed to read image {image_path}")
         return False
+
+    # Rescale the image if necessary
+    if rescale:
+        frame = rescale_image(frame)
 
     out_frame, out_frame_seg, out_frame_uv = predictor.predict(frame)
     
@@ -50,6 +63,9 @@ parser.add_argument(
 parser.add_argument(
     "-o", "--output", type=str, help="Set the output path to the folder for saving processed images", default="detected"
 )
+parser.add_argument(
+    "-r", "--rescale", action='store_true', help="Rescale images if their maximum dimension is larger than 512 pixels", default=False
+)
 args = parser.parse_args()
 
 logger = GetLogger.logger(__name__)
@@ -65,6 +81,13 @@ image_files = get_image_files(input_folder)
 n_images = len(image_files)
 logger.info(f"No of images {n_images}")
 
+# Check if GPU is available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if device.type == "cuda":
+    logger.info("Running on GPU")
+else:
+    logger.info("Running on CPU")
+
 # Process images in parallel
 done = 0
 
@@ -77,8 +100,8 @@ def update_progress(future):
     )
     sys.stdout.flush()
 
-with ThreadPoolExecutor() as executor:
-    futures = [executor.submit(process_image, image_file, input_folder, output_folder, predictor) for image_file in image_files]
+with ThreadPoolExecutor(max_workers=10) as executor:
+    futures = [executor.submit(process_image, image_file, input_folder, output_folder, predictor, args.rescale) for image_file in image_files]
     for future in futures:
         future.add_done_callback(update_progress)
 
